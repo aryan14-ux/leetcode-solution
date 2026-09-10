a7a994a11ae2f0630f80f5a66b85fcb8193e4581
Aryan Vishnoi <aryanvishnoi90@gmail.com>
2026-09-10 10:09:49 +0530

    Time: 0 ms (100%), Space: 95.2 MB (60.92%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -9,7 +9,7 @@ public:
         int low = 0;
         int high = n1;
         while (low <= high) {
-            int cut1 = (low + high) >> 1;
+            int cut1 = (low + high)/2;
             int cut2 = (n1 + n2 + 1) / 2 - cut1;
             int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
             int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
